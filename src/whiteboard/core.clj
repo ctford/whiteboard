9ed26bf3470196f70ac90077d8067bda7c2523f3
Chris Ford <christophertford@gmail.com>
2017-02-25 23:27:47 +0000

    Run a little state machine in each server.

--- src/whiteboard/core.clj
+++ src/whiteboard/core.clj
@@ -1,18 +1,35 @@
 (ns whiteboard.core
-  (:require [yada.yada :as yada]))
+  (:require [yada.yada :as yada]
+            [schema.core :as schema]))
+
+(def fsm
+  {"locked" {"unlock" "opened"}
+   "closed" {"lock"   "locked"
+             "open"   "opened"}
+   "opened" {"close"  "closed"}})
 
 (def servers (atom []))
 
+(defn respond [state context]
+  (when-let [action (get-in context [:parameters :query :action])]
+    (let [allowed-actions (fsm @state)]
+      (swap! state (partial get allowed-actions action))))
+  @state)
+
 (defn start-one! []
-  (let [server (yada/listener
+  (let [state (atom "locked")
+        server (yada/listener
                  ["/" (yada/resource {:produces "text/plain"
-                                      :response "Hello World!"})])]
+                                      :methods {:get
+                                                {:parameters
+                                                 {:query {(schema/optional-key :action) String}}}}
+                                      :response (partial respond state)})])]
     (swap! servers conj server)))
 
 (defn shut-down! []
   (doseq [{shut-down-one! :close} @servers]
     (shut-down-one!))
-  (reset! servers {}))
+  (reset! servers []))
 
 (comment
   (shut-down!)
