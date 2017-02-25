90700319d889dabc86e0f8b3a1c95951cac07ab0
Chris Ford <christophertford@gmail.com>
2017-02-25 22:24:35 +0000

    Manage a family of servers and shut them down.

--- src/whiteboard/core.clj
+++ src/whiteboard/core.clj
@@ -1,18 +1,21 @@
 (ns whiteboard.core
   (:require [yada.yada :as yada]))
 
-(defn start [port]
-  (yada/listener
-    ["/"
-     [["hello" (yada/resource {:produces "text/plain"
-                               :response "World!"})]
-      [true (yada/as-resource nil)]]]
-    {:port port}))
+(def servers (atom []))
+
+(defn start-one! []
+  (let [server (yada/listener
+                 ["/"
+                  [["hello" (yada/resource {:produces "text/plain"
+                                            :response "World!"})]
+                   [true (yada/as-resource nil)]]])]
+    (swap! servers conj server)))
+
+(defn shut-down! []
+  (doseq [{shut-down-one! :close} @servers]
+    (shut-down-one!))
+  (reset! servers {}))
 
 (comment
-
-  (def server (start 3000))
-
-  ((:close server))
-
- )
+  (shut-down!)
+  (start-one!))
